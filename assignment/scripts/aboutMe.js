2eaf97aeb5054b4021ce647636998d4178d12443
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-06-16 15:46:39 -0500

    fixed quotation marks on strings

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,11 +1,11 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 
-let firstName = kelsey;
+let firstName = 'kelsey';
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 
-let lastName = kuno;
+let lastName = 'kuno';
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
